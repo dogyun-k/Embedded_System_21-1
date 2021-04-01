fc2520044c24e0d9eff5811631b8a1bb5f30874f
dogyun-k <kdk124578@naver.com>
2021-04-01 12:56:28 +0900

    update main

--- 03_arm_cross_complie/main.c
+++ 03_arm_cross_complie/main.c
@@ -3,17 +3,9 @@
 int main(void){
 
     unsigned int led  = 0;
-    int count = 0;
 
     while(1){
-        if(count % 2 == 0){
-            led = 1;
-        }
-        else{
-            led = 0;
-        }
-        printf("led = %d\n", led);
-        count++;
+        led++;
     }
 
 
